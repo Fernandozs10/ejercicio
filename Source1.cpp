93170ab4e7dcb8e55fea26d5d369324419a836b5
Fernandozs10 <54364092+Fernandozs10@users.noreply.github.com>
2020-04-07 08:17:53 -0500

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() 
+{
+	int x;
+	int y;
+	std::cout << "ingrese el valor de la variable x : ";
+	std::cin >> x;
+	std::cout << "ingresa el valor de la variable y : ";
+	std::cin >> y;
+	int z = x + y;
+	int r = x - y;
+	int a = x * y;
+	std::cout << "la suma es "<< z << std::endl;
+	std::cout << "la resta es " << r << std::endl;
+	std::cout << "la multiplicacion es " << a << std::endl;
+	return 0;
+}
